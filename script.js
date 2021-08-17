285b5e5102cb4a832e9375bcbcd548e23eb3cee7
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-08-17 21:40:34 +0500

    DRY the code and check linter errors

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+/* eslint-disable no-undef */
+/* eslint-disable max-len */
 
 class BookCollection {
   constructor() {
@@ -18,20 +19,18 @@ class BookCollection {
     <li>${books.title}</li>
     <li>${books.author}</li>
     <button type='button' id='${books.id}' class='remove-btn'>Remove</button>`;
-  
+
   setItemFunc = () => {
     localStorage.setItem('BooksList', JSON.stringify(this.collection));
     this.bookList.innerHTML = `${this.collection.map(this.listBooks).join('')}`;
   };
-  
+
   removeBook = (e) => {
     const buttonId = e.target.id;
     this.collection = this.collection.filter((y) => y !== this.collection[this.collection.findIndex((x) => x.id === parseInt(buttonId, 10))]);
-    // localStorage.setItem('BooksList', JSON.stringify(books));
-    // bookList.innerHTML = `${books.map(listBooks).join('')}`;
     this.setItemFunc();
   };
-  
+
   removeButtonEventListener = () => {
     this.bookList.addEventListener('click', (e) => {
       if (e.target.classList.contains('remove-btn')) {
@@ -39,7 +38,7 @@ class BookCollection {
       }
     });
   };
-  
+
   addBooks = (e) => {
     e.preventDefault();
     this.i += 1;
@@ -49,13 +48,11 @@ class BookCollection {
       author: document.getElementById('author').value,
     };
     this.collection.push(localBook);
-    // localStorage.setItem('BooksList', JSON.stringify(books));
-    // bookList.innerHTML = `${books.map(listBooks).join('')}`;
     this.setItemFunc();
     this.removeButtonEventListener();
     this.form.reset();
   };
-  
+
   getBooks = () => {
     const getData = localStorage.getItem('BooksList');
     const data = JSON.parse(getData);
@@ -66,50 +63,12 @@ class BookCollection {
     this.removeButtonEventListener();
   };
 
-  // ShowListBooks = (arr) => {
-  //   const listBook = arr.map((book) => `
-  //   <li>${book.title}</li>
-  //   <li>${book.author}</li>
-  //   <button type='button' id='${book.id}' class='remove-button'>Remove</button>`).join('');
-  //   document.getElementById('bookList').innerHTML = listBook;
-  // }
-
-  // addBooks() {
-  //   i += 1;
-  //   const book = {
-  //     id: i,
-  //     title: document.getElementById('booktitle').value,
-  //     author: document.getElementById('authorname').value,
-  //   };
-  //   this.collection = [...this.collection, book];
-  //   localStorage.setItem('bookObject', JSON.stringify(this.collection));
-  //   this.ShowListBooks(this.collection);
-  //   document.getElementById('bookList').addEventListener('click', (e) => {
-  //     if (e.target.classList.contains('remove-button')) {
-  //       this.removeBooks(e);
-  //     }
-  //   });
-  //   form.reset();
-  // }
-
-  // removeBooks(ev) {
-  //   const buttonId = ev.target.id;
-  //   this.collection = this.collection.filter(
-  //     (y) => y !== this.collection[this.collection.findIndex(
-  //       (x) => x.id === parseInt(buttonId, 10),
-  //     )],
-  //   );
-  //   localStorage.setItem('bookObject', JSON.stringify(this.collection));
-  //   this.ShowListBooks(this.collection);
-  // }
-
   init = () => {
     const dataGet = localStorage.getItem('BooksList');
     const data = JSON.parse(dataGet);
     if (data) {
       this.collection = data;
     }
-    console.log(this.collection);
     this.setItemFunc();
     this.removeButtonEventListener();
   };
@@ -117,4 +76,3 @@ class BookCollection {
 
 const bookCol = new BookCollection();
 bookCol.listBooks(bookCol.collection);
-
